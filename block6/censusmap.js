98ff163d29d3da7a69cb72d51fe07a3fb2686ada
Witoldurbanski <witold.urbanski@gmx.at>
2014-12-04 11:05:13 +0100

    Update censusmap.js

--- block6/censusmap.js
+++ block6/censusmap.js
@@ -33,37 +33,3 @@ $.get("data/DataDict.txt", function(response) {
       .html(line.substr(10, 105).trim()));
   });
 });
-// !TUTORIAL #2
-
-// TUTORIAL #3
-// Add behaviour to dropdown
-$('#topics').change(function() {
-  wmsLayer.getSource().updateParams({
-    'viewparams': 'column:' + $('#topics>option:selected').val()
-  });
-});
-// !TUTORIAL #3
-
-// TUTORIAL #6
-// Create an ol.Overlay with a popup anchored to the map
-var popup = new ol.Overlay({
-  element: $('#popup')
-});
-olMap.addOverlay(popup);
-
-// Handle map clicks to send a GetFeatureInfo request and open the popup
-olMap.on('singleclick', function(evt) {
-  var view = olMap.getView();
-  var url = wmsLayer.getSource().getGetFeatureInfoUrl(evt.coordinate,
-      view.getResolution(), view.getProjection(), {'INFO_FORMAT': 'text/html'});
-  popup.setPosition(evt.coordinate);
-  $('#popup-content iframe').attr('src', url);
-  $('#popup')
-    .popover({content: function() { return $('#popup-content').html(); }})
-    .popover('show');
-  // Close popup when user clicks on the 'x'
-  $('.popover-title').click(function() {
-    $('#popup').popover('hide');
-  });
-});
-// !TUTORIAL #6
