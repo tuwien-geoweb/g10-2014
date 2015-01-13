77bf60b8ad0d549c38e0373a3fd0b30b1c4b24fb
SebastianVcelar <SebastianVcelar@users.noreply.github.com>
2015-01-13 14:45:59 +0100

    Update censusmap_us.js

--- uebung/censusmap_us.js
+++ uebung/censusmap_us.js
@@ -91,3 +91,8 @@ parseFloat(bbox[0]), parseFloat(bbox[3]), parseFloat(bbox[1])],
 xhr.send();
 evt.preventDefault();
 };
+view: new ol.View({
+center: ol.proj.transform([16.4, 48.2], 'EPSG:4326', 'EPSG:3857'),
+zoom: 11,
+maxZoom: 18
+})
