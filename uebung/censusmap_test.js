3902f18c13870a50b45d88fdd3fa4fa370c67a25
SebastianVcelar <SebastianVcelar@users.noreply.github.com>
2015-01-26 19:08:29 +0100

    Update censusmap_test.js

--- uebung/censusmap_test.js
+++ uebung/censusmap_test.js
@@ -202,15 +202,15 @@ olMap.on('singleclick', function(evt) {
 
 // Submit query to Nominatim and zoom map to the result's extent
 var form = document.forms[0];
-form.onsubmit = function(evt) {
-  var url = 'http://nominatim.openstreetmap.org/search?format=json&q=';
-  url += form.query.value;
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", url, true);
-  xhr.onload = function() {
-    var result = JSON.parse(xhr.responseText);
-    if (result.length > 0) {
-      var bbox = result[0].boundingbox;
+form.onsubmit = function(searching) {
+  var iadrr = 'http://nominatim.openstreetmap.org/search?format=json&q=';
+  iadrr += form.query.value;
+  var yhr = new XMLHttpRequest();
+  yhr.open("GET", iadrr, true);
+  yhr.onload = function() {
+    var erg = JSON.parse(yhr.responseText);
+    if (erg.length > 0) {
+      var bbox = erg[0].boundingbox;
       olMap.getView().fitExtent(ol.proj.transform([parseFloat(bbox[2]),
           parseFloat(bbox[0]), parseFloat(bbox[3]), parseFloat(bbox[1])],
           'EPSG:4326', 'EPSG:3857'), olMap.getSize());
@@ -218,8 +218,8 @@ form.onsubmit = function(evt) {
     }
      
   };
-  xhr.send();
-  evt.preventDefault();
+  yhr.send();
+  searching.preventDefault();
 };
 
 
