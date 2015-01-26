e155f6e5a9a8c9d236ca347aa70a0f8951d0e10a
SebastianVcelar <SebastianVcelar@users.noreply.github.com>
2015-01-26 19:31:10 +0100

    Update censusmap_test.js

--- uebung/censusmap_test.js
+++ uebung/censusmap_test.js
@@ -18,9 +18,23 @@ var markerLayer = new ol.layer.Vector({
             }))
       })
 })
+var commentLayer = new ol.layer.Vector({
+  source: new ol.source.GeoJSON({
+    url: 'http://student.ifip.tuwien.ac.at/geoserver/g10_2014/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=g10_2014:comments&maxFeatures=50&outputFormat=json',
+    projection: 'EPSG:3857'
+  }),
+    style: new ol.style.Style({
+       image: new ol.style.Icon({
+          src: 'http://student.ifip.tuwien.ac.at/geoweb/2014/g10/website_g10/red.png',
+        })
+    })
+});
+
+
+
 var olMap = new ol.Map({
   target: 'map',
-  layers: [osmLayer, wmsLayer, markerLayer],
+  layers: [osmLayer, wmsLayer, markerLayer, commentLayer],
   view: new ol.View({
   center: ol.proj.transform([16.4, 48.2], 'EPSG:4326', 'EPSG:3857'),
   zoom: 11,
